42a89ec1d39f8c990d5c4520e2bc55282babf43c
Zijie Cai <leo20020208@gmail.com>
2024-06-24 20:24:28 -0400

    Add files via upload

--- mobile_game.py
+++ mobile_game.py
@@ -96,6 +96,8 @@ class N_Queens_Game:
         )
 
         self.output = Output()
+
+    def setup(self):
         self.reset = Button(description="New", layout=Layout(width="120px"))
         self.reset.on_click(self.new_reset)
 
